7e17ce7594c46c2aec0e1c838aee74d8b65112bd
xinghao <Xinghao@airarena.net>
2010-11-10 17:28:56 +1000

    commit

--- app/models/lastfm_data_source_handler.rb
+++ app/models/lastfm_data_source_handler.rb
@@ -136,7 +136,7 @@ class LastfmDataSourceHandler < DataSource
   end #end of function 
 
   def analyzeSimilarTrackRawDataImp track
-    websource = WebsourceTrackSimilarTempLastfm.find(:first, :conditions =>["altnet_id = ?", track.id], :order => "id desc")
+    websource = WebsourceTrackSimilarTemp120140Lastfm.find(:first, :conditions =>["altnet_id = ?", track.id], :order => "id desc")
     #puts websource.html
     document = Hpricot(websource.html.to_s)
     sarts = document.search("tr").search("//td[@class='subjectCell']")
--- test/similar_track_lastfm/analyze_7.rb
+++ test/similar_track_lastfm/analyze_7.rb
@@ -5,7 +5,7 @@
 # cron6:  1,200,000 - 1,6000,000
 
 lm = LastfmDataSourceHandler.new
-lm.analyzeSimilarTrackRawData(1160000,10000)
+lm.analyzeSimilarTrackRawData(1320000,20000)
 #lm.getWebRawSimilarTrackData(1300000,100000)
 #lm.getWebRawSimilarTrackData(1400000,100000)
 #lm.getWebRawSimilarTrackData(1500000,100000)
