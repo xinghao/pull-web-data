2d886bbd57380bf12241c137c2d17417d09a084c
xinghao <Xinghao@airarena.net>
2010-11-15 14:37:31 +1000

    add popularity

--- db/migrate/20101114083818_update_similar_tracks_popularity.rb
+++ db/migrate/20101114083818_update_similar_tracks_popularity.rb
@@ -1,10 +1,16 @@
 class UpdateSimilarTracksPopularity < ActiveRecord::Migration
   def self.up
-    # Track.find(:all, :conditions =>["is_valid = 1"] ).each do |track|
-    #   puts "processing:" + track.id.to_s
-    #   sql = "update similar_tracks set track_popularity = " + track.play_count.to_s + "/400677 where similar_track_id = " + track.id.to_s; 
-    #   execute sql 
-    # end    
+    Track.find(:all, :conditions =>["is_valid = 1"] ).each do |track|
+      puts "processing:" + track.id.to_s
+      sql = "update similar_tracks set track_popularity = " + track.play_count.to_s + "/400677 where similar_track_id = " + track.id.to_s; 
+      execute sql 
+    end
+    
+      puts "add popularity done"
+      puts "aggrageting appearance times.........."
+      sql = "update similar_tracks set track_popularity = track_popularity + appearance_times"
+      execute sql
+    
   end
 
   def self.down
