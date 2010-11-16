bab76d21ac1204956e22d1b78dfaf896ebbf128d
xinghao <Xinghao@airarena.net>
2010-11-16 17:06:00 +1000

    commit

---
+++ test/popular_track_source2/popular_track_cron2_2.rb
@@ -0,0 +1,11 @@
+# cron2:  0 - 200,000
+# cron3:  200,000 - 400,000
+# cron4:  400,000 - 700,000
+# cron7:  700,000 - 900,00
+# cron9:  900,000 - 110,000
+# cron5:  ??
+e = EchonestDataSourceHandler.new
+e.getWebRawTrackPopularData(100000,100000)
+
+
+
